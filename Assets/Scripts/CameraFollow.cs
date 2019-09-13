068ef3c868192c1d3f89cea6becdfd5efeaa75d3
Björn Åhström <ahstrom_81@hotmail.com>
2019-09-13 12:51:29 +0200

    first commit, added movment to player and start to build first level

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+
+    public Transform player;
+    public Transform leftBounds;
+    public Transform rightBounds;
+
+    public float smoothDampTime = 0.15f;
+
+    private Vector3 SmoothDampVelocity = Vector3.zero;
+
+    private float camWidth, camHeight, levelMin, levelMax;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        camHeight = Camera.main.orthographicSize * 2;
+        camWidth = camHeight * Camera.main.aspect;
+
+        float leftBoundsWidth = leftBounds.GetComponentInChildren<SpriteRenderer>().bounds.size.x / 2;
+        float tightBoundsWidth = rightBounds.GetComponentInChildren<SpriteRenderer>().bounds.size.x / 2;
+
+        levelMin = leftBounds.position.x + leftBoundsWidth + (camWidth / 2);
+        levelMax = rightBounds.position.x - tightBoundsWidth - (camWidth / 2);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (player)
+        {
+            float playerX = Mathf.Max(levelMin, Mathf.Min(levelMax, player.position.x));
+
+            float x = Mathf.SmoothDamp(transform.position.x, playerX, ref SmoothDampVelocity.x, smoothDampTime);
+
+            transform.position = new Vector3(x, transform.position.y, transform.position.z);
+        }
+        
+    }
+
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Vector2 moveVelocity;
+    public float jumpVelocity;
+    public float fallMultiplier = 2.5f;
+    public float lowJumpMultiplier = 2f;
+
+    private bool walk, walkLeft, walkRight, jump;
+
+    Rigidbody2D rb;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Jump();
+        CheckPlayerInput();
+        UpdatePlayerPosition();
+    }
+
+    void Jump()
+    {
+        if (jump)
+        {
+            GetComponent<Rigidbody2D>().velocity = Vector2.up * jumpVelocity;
+        }
+
+        if (rb.velocity.y < 0)
+        {
+            rb.velocity += Vector2.up * Physics2D.gravity.y * (fallMultiplier - 1) * Time.deltaTime;
+        }
+        else if (rb.velocity.y > 0 && !jump)
+        {
+            rb.velocity += Vector2.up * Physics2D.gravity.y * (lowJumpMultiplier - 1) * Time.deltaTime;
+        }
+    }
+
+    void UpdatePlayerPosition()
+    {
+        //Vector3 pos = transform.localPosition;
+        Vector3 scale = transform.localScale;
+
+        if (walk)
+        {
+            if (walkLeft)
+            {
+                rb.velocity = Vector2.left * moveVelocity;
+                //pos.x -= moveVelocity.x * Time.deltaTime;
+
+                //scale.x = -1;
+            }
+
+            else if (walkRight)
+            {
+                rb.velocity = Vector2.right * moveVelocity;
+                //pos.x += moveVelocity.x * Time.deltaTime;
+
+                //scale.x = 1;
+            }
+
+            //transform.position = pos;
+        }
+    }
+
+    void CheckPlayerInput()
+    {
+        bool inputLeft = Input.GetKey(KeyCode.LeftArrow);
+        bool inputRight = Input.GetKey(KeyCode.RightArrow);
+        bool inputJump = Input.GetKey(KeyCode.Space);
+
+        walk = inputLeft || inputRight;
+
+        walkLeft = inputLeft && !inputRight;
+        walkRight = !inputLeft && inputRight;
+
+        jump = inputJump;
+    }
+}
